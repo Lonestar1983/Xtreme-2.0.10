bc536fdf1a4e26adf7438166078aceb5a1cae6db
Shaun <crazycoder@live.co.uk>
2023-03-02 15:12:54 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,117 +17,115 @@
 
 /*****[CHANGES]**********************************************************
 -=[Base]=-
-      Nuke Patched                             v3.1.0       06/26/2005
-      Debugger                                 v1.0.0       11/14/2005
-      Auto Optimize                            v1.0.0       11/19/2005
-      Module Simplifications                   v1.0.0       11/19/2005
+	  Nuke Patched                             v3.1.0       06/26/2005
+	  Debugger                                 v1.0.0       11/14/2005
+	  Auto Optimize                            v1.0.0       11/19/2005
+	  Module Simplifications                   v1.0.0       11/19/2005
 -=[Other]=-
-      DB Connector                             v1.0.0       06/07/2005
-      Queries Count                            v2.0.1       08/21/2005
+	  DB Connector                             v1.0.0       06/07/2005
+	  Queries Count                            v2.0.1       08/21/2005
 -=[Mod]=-
-      Admin Icon/Link Pos                      v1.0.0       06/07/2005
+	  Admin Icon/Link Pos                      v1.0.0       06/07/2005
 	  NSN Center Blocks                        v2.2.1       05/26/2009
  ************************************************************************/
 
-if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
-    exit('Access Denied');
+if ( realpath( __FILE__ ) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+	exit( 'Access Denied' );
 }
 
-if(!defined('HEADER')) return;
+if ( ! defined( 'HEADER' ) ) {
+	return;
+}
 
-define('NUKE_FOOTER', true);
+define(' NUKE_FOOTER', true );
 
 function footmsg() {
-    global $foot1, $foot2, $foot3, $copyright, $total_time, $start_time, $footmsg, $db, $queries_count, $use_cache, $usrclearcache, $debugger, $debug, $cache, $use_cache, $start_mem;
-    static $has_echoed;
-    if(isset($has_echoed) && $has_echoed == 1) { return; }
-        $footmsg = "<span class=\"footmsg\"><br />\n";
-        if (!empty($foot1)) {
-            $footmsg .= $foot1."<br />\n";
-        }
-        if (!empty($foot2)) {
-            $footmsg .= $foot2."<br />\n";
-        }
-        if (!empty($foot3)) {
-            $footmsg .= $foot3."<br />\n";
-        }
+	global $foot1, $foot2, $foot3, $copyright, $total_time, $start_time, $footmsg, $db, $queries_count, $use_cache, $usrclearcache, $debugger, $debug, $cache, $use_cache, $start_mem;
+	static $has_echoed;
 
-        // DO NOT REMOVE THE FOLLOWING COPYRIGHT LINE. YOU'RE NOT ALLOWED TO REMOVE NOR EDIT THIS.
-        // IF YOU REALLY NEED TO REMOVE IT AND HAVE MY WRITTEN AUTHORIZATION CHECK:
-        // http://phpnuke.org/modules.php?name=Commercial_License
-        // PLAY FAIR AND SUPPORT THE DEVELOPMENT, PLEASE!
-        $footmsg .= '<br />'.$copyright.'<br />';
-        $footmsg = (preg_match(HEX_PREG,$footmsg)) ? $footmsg : $footmsg."<br />".ord_crypt_decode(HEX_CACHED)."<br />";
-        if($use_cache && $usrclearcache) {
-            $footmsg .= "<form method='post' name='clear_cache' action='".$_SERVER['REQUEST_URI']."'>";
-            $footmsg .= "<input type='hidden' name='clear_cache' value='1'><span style='font-size: 11px'>";
-            $footmsg .= _SITECACHED . "</span> <a href=\"javascript:clear_cache.submit()\">" . _UPDATECACHE . "</a>";
-            $footmsg .= "</form>";
-        }
-        $total_time = (get_microtime() - $start_time);
-        $total_time = '<span class="copyright">[ '._PAGEGENERATION." ".substr($total_time,0,4)." "._SECONDS;
-        if ($start_mem > 0) {
-            $total_mem = memory_get_usage()-$start_mem;
-            $total_time .= ' | Memory Usage: '.(($total_mem >= 1048576) ? round((round($total_mem / 1048576 * 100) / 100), 2).' MB' : (($total_mem >= 1024) ? round((round($total_mem / 1024 * 100) / 100), 2).' KB' : $total_mem.' Bytes'));
-        }
-/*****[BEGIN]******************************************
- [ Other:   Queries Count                      v2.0.1 ]
- ******************************************************/
-        if($queries_count) {
-            $total_time .= ' | DB Queries: ' . $db->num_queries;
-        }
-/*****[END]********************************************
- [ Other:   Queries Count                      v2.0.1 ]
- ******************************************************/
-        $total_time .= ' ]';
-        $total_time .= '</span><br />';
-/*****[BEGIN]******************************************
- [ Base:    Auto Optimize                      v1.0.0 ]
- ******************************************************/
-        if(is_admin()) {
-            $first_time = false;
-            if (($last_optimize = $cache->load('last_optimize', 'config')) === false) {
-                $last_optimize = time();
-                $first_time = true;
-            }
-            //For information on how to change the auto-optimize intervals
-            //Please see www.php.net/strtotime
-            //Default: -1 day
-            $interval = strtotime('-1 day');
-            if (($last_optimize <= $interval) || ($first_time && $cache->valid && $use_cache))
-            {
-                if ($db->sql_optimize()) {
-                    $cache->save('last_optimize', 'config', time());
-                    $total_time .= "<br />Database Optimized";
-                }
-            }
-/*****[BEGIN]******************************************
- [ Base:    Module Simplifications             v1.0.0 ]
- ******************************************************/
-            update_modules();
-/*****[END]********************************************
- [ Base:    Module Simplifications             v1.0.0 ]
- ******************************************************/
-        }
-/*****[END]********************************************
- [ Base:    Auto Optimize                      v1.0.0 ]
- ******************************************************/
-            $footmsg .= $total_time."<br />\n</span>\n";
-/*****[BEGIN]******************************************
- [ Base:    Debugger                           v1.0.0 ]
- ******************************************************/
-        if(is_admin() && $debugger->debug && count($debugger->errors) > 0) {
-            $footmsg .= "<br /><center><strong>Debugging:</strong></center>";
-            $footmsg .= "<table border='0' width='80%' align='center'><tr><td>";
-            $footmsg .= $debugger->return_errors();
-            $footmsg .= "</td></tr></table>";
-        }
-        if (is_admin()) {
-            echo $db->print_debug();
-        }
-/*****[END]********************************************
- [ Base:    Debugger                           v1.0.0 ]
- ******************************************************/
+	if ( isset( $has_echoed ) && $has_echoed == 1 ) {
+		return;
+	}
+
+	$footmsg = "<span class=\"footmsg\"><br />\n";
+
+	if ( ! empty( $foot1 ) ) {
+		$footmsg .= $foot1 . "<br />\n";
+	}
+
+	if ( ! empty( $foot2 ) ) {
+		$footmsg .= $foot2 . "<br />\n";
+	}
+
+	if ( ! empty( $foot3 ) ) {
+		$footmsg .= $foot3 . "<br />\n";
+	}
+
+	// DO NOT REMOVE THE FOLLOWING COPYRIGHT LINE. YOU'RE NOT ALLOWED TO REMOVE NOR EDIT THIS.
+	// IF YOU REALLY NEED TO REMOVE IT AND HAVE MY WRITTEN AUTHORIZATION CHECK:
+	// http://phpnuke.org/modules.php?name=Commercial_License
+	// PLAY FAIR AND SUPPORT THE DEVELOPMENT, PLEASE!
+	$footmsg .= '<br />' . $copyright . '<br />';
+	$footmsg = ( preg_match( HEX_PREG, $footmsg ) ) ? $footmsg : $footmsg . "<br />" . ord_crypt_decode( HEX_CACHED ) . "<br />";
+
+	if ( $use_cache && $usrclearcache ) {
+		$footmsg .= "<form method='post' name='clear_cache' action='" . $_SERVER['REQUEST_URI'] . "'>";
+		$footmsg .= "<input type='hidden' name='clear_cache' value='1'><span style='font-size: 11px'>";
+		$footmsg .= _SITECACHED . "</span> <a href=\"javascript:clear_cache.submit()\">" . _UPDATECACHE . "</a>";
+		$footmsg .= "</form>";
+	}
+
+	$total_time = ( get_microtime() - $start_time );
+	$total_time = '<span class="copyright">[ ' . _PAGEGENERATION . " " . substr( $total_time, 0, 4 ) . " " . _SECONDS;
+	if ( $start_mem > 0 ) {
+		$total_mem = memory_get_usage() - $start_mem;
+		$total_time .= ' | Memory Usage: '.(($total_mem >= 1048576) ? round((round($total_mem / 1048576 * 100) / 100), 2).' MB' : (($total_mem >= 1024) ? round((round($total_mem / 1024 * 100) / 100), 2).' KB' : $total_mem.' Bytes'));
+	}
+
+	if ( $queries_count ) {
+		$total_time .= ' | DB Queries: ' . $db->num_queries;
+	}
+
+	$total_time .= ' ]';
+	$total_time .= '</span><br />';
+
+	if ( is_admin() ) {
+		$first_time = false;
+
+		if ( ( $last_optimize = $cache->load( 'last_optimize', 'config' ) ) === false ) {
+			$last_optimize = time();
+			$first_time    = true;
+		}
+
+		/**
+		 * For information on how to change the auto-optimize intervals
+		 * Please see www.php.net/strtotime
+		 * Default: -1 day
+		 */
+		$interval = strtotime( '-1 day' );
+
+		if ( ( $last_optimize <= $interval ) || ( $first_time && $cache->valid && $use_cache ) ) {
+			if ( $db->sql_optimize() ) {
+				$cache->save( 'last_optimize', 'config', time() );
+				$total_time .= '<br />Database Optimized';
+			}
+		}
+
+		update_modules();
+	}
+
+	$footmsg .= $total_time . "<br />\n</span>\n";
+
+	if ( is_admin() && $debugger->debug && count( $debugger->errors ) > 0 ) {
+		$footmsg .= "<br /><center><strong>Debugging:</strong></center>";
+		$footmsg .= "<table border='0' width='80%' align='center'><tr><td>";
+		$footmsg .= $debugger->return_errors();
+		$footmsg .= "</td></tr></table>";
+	}
+	if ( is_admin() ) {
+		echo $db->print_debug();
+	}
+
 	$debug_sql = false;
 	if (is_admin() && !is_bool($debug) && $debug == 'full') {
 		$strstart = strlen(NUKE_BASE_DIR);
@@ -144,27 +142,25 @@ function footmsg() {
 	echo $debug_sql;
 	unset($debug_sql);
 	global $browser;
-	if ($browser == 'Bot' || $browser == 'Other') 
-    {
-        $footmsg .= '<span style="display:none;"><a href="includes/trap.php">Do Not Click</a></span>'.PHP_EOL;
-    }
+	if ($browser == 'Bot' || $browser == 'Other') {
+		$footmsg .= '<span style="display:none;"><a href="includes/trap.php">Do Not Click</a></span>'.PHP_EOL;
+	}
 	echo $footmsg;
-    $has_echoed = 1;
+	$has_echoed = 1;
 }
 
 /*****[BEGIN]******************************************
  [ Base:    Admin Icon/Link Pos                v1.0.0 ]
  ******************************************************/
-if ( defined('ADMIN_FILE') && defined('ADMIN_POS') && is_admin())
-{
-    global $admin;
-    $admin1 = base64_decode($admin);
-    $admin1 = addslashes($admin1);
-    $admin1 = explode(':', $admin1);
-    $aid = $admin1[0];
-    unset($admin1);
-    echo "<br />";
-    GraphicAdmin(0);
+if ( defined( 'ADMIN_FILE' ) && defined( 'ADMIN_POS' ) && is_admin() ) {
+	global $admin;
+	$admin1 = base64_decode( $admin );
+	$admin1 = addslashes( $admin1 );
+	$admin1 = explode( ':', $admin1 );
+	$aid    = $admin1[0];
+	unset( $admin1 );
+	echo '<br />';
+	GraphicAdmin( 0 );
 }
 /*****[END]********************************************
  [ Base:    Admin Icon/Link Pos                v1.0.0 ]
@@ -172,7 +168,7 @@ if ( defined('ADMIN_FILE') && defined('ADMIN_POS') && is_admin())
 global $prefix, $user_prefix, $db, $index, $user, $cookie, $storynum, $user, $cookie, $Default_Theme, $foot1, $foot2, $foot3, $foot4, $home, $name, $admin, $persistency, $do_gzip_compress, $cache;
 
 if ( defined( 'HOME_FILE' ) ) {
-    blocks( 'Down' );
+	blocks( 'Down' );
 
 	// If you want either of the following on all pages simply
 	// move the include to before if (defined('HOME_FILE'))
@@ -183,21 +179,21 @@ if ( defined( 'HOME_FILE' ) ) {
 }
 
 if ( ! defined( 'HOME_FILE' ) AND defined( 'MODULE_FILE' ) AND file_exists( NUKE_MODULES_DIR . $name . '/copyright.php' ) ) {
-    $cpname = str_replace( '_', ' ', $name );
-    echo "<div align=\"right\"><a href=\"javascript:openwindow()\">$cpname &copy;</a></div>";
+	$cpname = str_replace( '_', ' ', $name );
+	echo "<div align=\"right\"><a href=\"javascript:openwindow()\">$cpname &copy;</a></div>";
 }
 
 if ( ! defined( 'HOME_FILE' ) AND defined( 'MODULE_FILE' ) AND ( file_exists( NUKE_MODULES_DIR . $name . '/admin/panel.php' ) && is_admin() ) ) {
-    echo '<br />';
-    opentable();
-    include_once NUKE_MODULES_DIR . $name . '/admin/panel.php';
-    closetable();
+	echo '<br />';
+	opentable();
+	include_once NUKE_MODULES_DIR . $name . '/admin/panel.php';
+	closetable();
 }
 
 themefooter();
 
 if ( file_exists( NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php' ) ) {
-    include_once NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php';
+	include_once NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php';
 }
 
 writeBODYJS();
@@ -207,16 +203,16 @@ $cache->resync();
 $db->sql_close();
 
 if ( GZIPSUPPORT && $do_gzip_compress ) {
-    $gzip_contents = ob_get_contents();
-    ob_end_clean();
-    $gzip_size     = strlen( $gzip_contents );
-    $gzip_crc      = crc32( $gzip_contents );
-    $gzip_contents = gzcompress( $gzip_contents, 9 );
-    $gzip_contents = substr( $gzip_contents, 0, strlen( $gzip_contents ) - 4 );
-    echo "\x1f\x8b\x08\x00\x00\x00\x00\x00";
-    echo $gzip_contents;
-    echo pack( 'V', $gzip_crc );
-    echo pack( 'V', $gzip_size );
+	$gzip_contents = ob_get_contents();
+	ob_end_clean();
+	$gzip_size     = strlen( $gzip_contents );
+	$gzip_crc      = crc32( $gzip_contents );
+	$gzip_contents = gzcompress( $gzip_contents, 9 );
+	$gzip_contents = substr( $gzip_contents, 0, strlen( $gzip_contents ) - 4 );
+	echo "\x1f\x8b\x08\x00\x00\x00\x00\x00";
+	echo $gzip_contents;
+	echo pack( 'V', $gzip_crc );
+	echo pack( 'V', $gzip_size );
 }
 ob_end_flush();
 exit;
