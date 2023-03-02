bd9d3e0d07302490861724dea9f457bc7161db61
Shaun <crazycoder@live.co.uk>
2023-03-02 15:06:17 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -171,70 +171,52 @@ if ( defined('ADMIN_FILE') && defined('ADMIN_POS') && is_admin())
  ******************************************************/
 global $prefix, $user_prefix, $db, $index, $user, $cookie, $storynum, $user, $cookie, $Default_Theme, $foot1, $foot2, $foot3, $foot4, $home, $name, $admin, $persistency, $do_gzip_compress, $cache;
 
-/*****[BEGIN]******************************************
- [ Mod:    NSN Center Blocks                   v2.2.1 ]
- ******************************************************/
-if (defined('HOME_FILE')) {
-    blocks('Down');
-	
+if ( defined( 'HOME_FILE' ) ) {
+    blocks( 'Down' );
+
 	// If you want either of the following on all pages simply
 	// move the include to before if (defined('HOME_FILE'))
 	//
 	// Visit www.evolution-xtreme.com for support if your stuck
-	include(NUKE_INCLUDE_DIR.'cblocks3.php');
-	include(NUKE_INCLUDE_DIR.'cblocks4.php');
+	include NUKE_INCLUDE_DIR . 'cblocks3.php';
+	include NUKE_INCLUDE_DIR . 'cblocks4.php';
 }
-/*****[END]********************************************
- [ Mod:    NSN Center Blocks                   v2.2.1 ]
- ******************************************************/
 
-if (!defined('HOME_FILE') AND defined('MODULE_FILE') AND file_exists(NUKE_MODULES_DIR.$name.'/copyright.php')) {
-    $cpname = str_replace("_", " ", $name);
+if ( ! defined( 'HOME_FILE' ) AND defined( 'MODULE_FILE' ) AND file_exists( NUKE_MODULES_DIR . $name . '/copyright.php' ) ) {
+    $cpname = str_replace( '_', ' ', $name );
     echo "<div align=\"right\"><a href=\"javascript:openwindow()\">$cpname &copy;</a></div>";
 }
 
-if (!defined('HOME_FILE') AND defined('MODULE_FILE') AND (file_exists(NUKE_MODULES_DIR.$name.'/admin/panel.php') && is_admin())) {
-    echo "<br />";
-    OpenTable();
-    include_once(NUKE_MODULES_DIR . $name . '/admin/panel.php');
-    CloseTable();
+if ( ! defined( 'HOME_FILE' ) AND defined( 'MODULE_FILE' ) AND ( file_exists( NUKE_MODULES_DIR . $name . '/admin/panel.php' ) && is_admin() ) ) {
+    echo '<br />';
+    opentable();
+    include_once NUKE_MODULES_DIR . $name . '/admin/panel.php';
+    closetable();
 }
+
 themefooter();
-// if (!defined('IN_PHPBB')) {
-//     echo "<div style=\"display:none\" id=\"resizemod\"></div>";
-// }
-if (file_exists(NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php')) {
-    include_once(NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php');
+
+if ( file_exists( NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php' ) ) {
+    include_once NUKE_INCLUDE_DIR . 'custom_files/custom_footer.php';
 }
 
 writeBODYJS();
 echo '  </body>';
 echo '</html>';
 $cache->resync();
-/*****[BEGIN]******************************************
- [ Other:   DB Connector                       v1.0.0 ]
- [ Other:   Persistent DB Connection           v1.0.0 ]
- ******************************************************/
 $db->sql_close();
-/*****[END]********************************************
- [ Other:   Persistent DB Connection           v1.0.0 ]
- [ Other:   DB Connector                       v1.0.0 ]
- ******************************************************/
-// $s = 'PHP-Nuke Copyright &copy; 2006 by Francisco Burzi.<br />All logos, trademarks and posts in this site are property of their respective owners, all the rest &copy; 2006 by the site owner.<br />Powered by <a href="http://www.nuke-evolution.com" target="_blank">Nuke-Evolution</a>';
 
-if(GZIPSUPPORT && $do_gzip_compress) {
+if ( GZIPSUPPORT && $do_gzip_compress ) {
     $gzip_contents = ob_get_contents();
     ob_end_clean();
-    $gzip_size = strlen($gzip_contents);
-    $gzip_crc = crc32($gzip_contents);
-    $gzip_contents = gzcompress($gzip_contents, 9);
-    $gzip_contents = substr($gzip_contents, 0, strlen($gzip_contents) - 4);
+    $gzip_size     = strlen( $gzip_contents );
+    $gzip_crc      = crc32( $gzip_contents );
+    $gzip_contents = gzcompress( $gzip_contents, 9 );
+    $gzip_contents = substr( $gzip_contents, 0, strlen( $gzip_contents ) - 4 );
     echo "\x1f\x8b\x08\x00\x00\x00\x00\x00";
     echo $gzip_contents;
-    echo pack('V', $gzip_crc);
-    echo pack('V', $gzip_size);
+    echo pack( 'V', $gzip_crc );
+    echo pack( 'V', $gzip_size );
 }
 ob_end_flush();
 exit;
-
-?>
